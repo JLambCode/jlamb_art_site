719b4bd6f4192969ee5e09655b01cfb273551ada
SaveJL101 <66347648+SaveJL101@users.noreply.github.com>
2023-01-15 13:50:15 -0500

    nav stuff

--- trunk/includes/nav.php
+++ trunk/includes/nav.php
@@ -1,3 +1,6 @@
 <nav class="navbar">
-    <a href="landing.php" class="navbar-brand"><img src="" alt="JLamb Art Logo"></a>
+    <div class="container-fluid">
+        <a href="landing.php" class="navbar-brand"><img src="" alt="JLamb Art Logo"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    </div>
 </nav>
