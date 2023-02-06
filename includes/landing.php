ac596779a2f68d823beffeb82f15160ff04d0c08
Jeremy <66347648+JLambCode@users.noreply.github.com>
2023-02-06 15:42:51 -0500

    added styling to landing images

--- includes/landing.php
+++ includes/landing.php
@@ -1,13 +1,13 @@
 <div class="container-fluid text-center my-3">
     <div class="row justify-content-evenly">
-        <div class="col p-1">
-            <img src="img/joe_landing.jpg" class="img landing-img" alt="Joe's Space">
+        <div class="col p-1 landing-img-div">
+            <img src="img/joe_landing.jpg" class="landing-img" alt="Joe's Space">
         </div>
-        <div class="col p-1">
-            <img src="img/juliana_landing.jpg" class="img landing-img" alt="Juliana's Space">
+        <div class="col p-1 landing-img-div">
+            <img src="img/juliana_landing.jpg" class="landing-img" alt="Juliana's Space">
         </div>
-        <div class="col p-1">
-            <img src="img/jeremy_landing.png" class="img landing-img" alt="Jeremy's Space">
+        <div class="col p-1 landing-img-div">
+            <img src="img/jeremy_landing.png" class="landing-img" alt="Jeremy's Space">
         </div>
     </div>
 </div>
